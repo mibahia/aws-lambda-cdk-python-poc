a07b1c0664df8e0bb65cf45fe5ca8aa48484b198
mibahia <mariaizabel.bahia@gmail.com>
2026-02-06 11:48:45 +0000

    remove env var

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ from py_cdk_poc.py_cdk_poc_stack import UploadRawData
 
 app = cdk.App()
 
-env_name = app.node.try_get_context("env")
-
 UploadRawData(
     app,
-    f"upload-raw-data-{env_name}",  # this is the stack name
+    "upload-raw-data",  # this is the stack name
     env=cdk.Environment(account="", region="eu-west-2"),
-    env_name=env_name,
 )
 
 app.synth()
